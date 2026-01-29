53412f44309db50dfeb7269b6f7cd78c762bf291
Kereszturi Balázs <diak@petrik.hu>
2026-01-29 13:52:23 +0100

    Add project files.

---
+++ Digitalis Nyomozas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Digitalis_Nyomozas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
