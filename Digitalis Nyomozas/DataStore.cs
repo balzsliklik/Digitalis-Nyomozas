511f3d8210a4b9caa8f5966696d4e9655ec7a95a
Balázs Kereszturi <kereszturi.balazs@petrik.hu>
2026-02-01 15:20:18 +0100

    ottho

--- Digitalis Nyomozas/DataStore.cs
+++ Digitalis Nyomozas/DataStore.cs
@@ -8,5 +8,40 @@ namespace Digitalis_Nyomozas
 {
 	internal class DataStore
 	{
-	}
+        private List<User> felhasznalok;
+        private List<Case> ugyek;
+        private List<Person> szemelyek;
+        private List<Evidence> bizonyitekok;
+
+        internal List<User> Felhasznalok { get => felhasznalok; }
+        internal List<Case> Ugyek { get => ugyek; }
+        internal List<Person> Szemelyek { get => szemelyek; }
+        internal List<Evidence> Bizonyitekok { get => bizonyitekok;}
+
+        public DataStore()
+        {
+            felhasznalok = new List<User>();
+            ugyek = new List<Case>();
+            szemelyek = new List<Person>();
+            bizonyitekok = new List<Evidence>();
+        }
+
+        public void AddUser(User user)
+        {
+            felhasznalok.Add(user);
+
+        }
+        public void AddCase(Case c)
+        {
+            ugyek.Add(c);
+        }
+        public void AddPerson(Person p)
+        {
+            szemelyek.Add(p);
+        }
+        public void AddEvidence(Evidence e)
+        {
+            bizonyitekok.Add(e);
+        }
+    }
 }
--- Digitalis Nyomozas/DecisionEngine.cs
+++ Digitalis Nyomozas/DecisionEngine.cs
@@ -8,5 +8,19 @@ namespace Digitalis_Nyomozas
 {
 	internal class DecisionEngine
 	{
+		public void Evaluation(Suspect gyanusitott, Evidence bizonyitek)
+		{
+			int vmi = bizonyitek.MegbizhatosagiErtek * 10;
+			vmi += gyanusitott.GyanusitottSzint;
+			if (vmi >= 80)
+			{
+				Console.WriteLine("gyanusitott elérte a kritikus pontot, keresztrefeszítest érdemel!");
+			}
+			else
+			{
+				Console.WriteLine("több információra van szükség gyanusított elítéléséhez");
+			}
+			
+		}
 	}
 }
