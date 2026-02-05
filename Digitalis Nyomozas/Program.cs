be4bb8f7a0e8bfc75929d222b6bdb1161a5b667e
Petrik Diak <diak@petrik.hu>
2026-02-05 15:01:09 +0100

    su li

--- Digitalis Nyomozas/Program.cs
+++ Digitalis Nyomozas/Program.cs
@@ -10,7 +10,7 @@ namespace Digitalis_Nyomozas
         static EvidenceManager evidenceManager = new EvidenceManager();
 
 
-		static string[] menu = { "Ügyek kezelése", "Személyek kezelése", "Bizonyítékok kezelése", "Bizonyítékok kezelése", "Idővonal megtekintése", "Elemzés / döntések", "Kilépés" };
+        static string[] menu = { "Ügyek kezelése", "Személyek kezelése", "Bizonyítékok kezelése", "Bizonyítékok kezelése", "Idővonal megtekintése", "Elemzés / döntések", "Kilépés" };
 
 
         static void ugykezeles()
@@ -20,40 +20,72 @@ namespace Digitalis_Nyomozas
 
             while (fut)
             {
-				Console.WriteLine(menu[0]);
-				Console.WriteLine("\n1. Listázas\n2. Ügy hozzáadása\n3.Vissza");
-				int input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(menu[0]);
+                Console.WriteLine("\n1. Listázas\n2. Ügy hozzáadása\n3.Vissza");
+                int input = Convert.ToInt32(Console.ReadLine());
 
-				if (input <1 || input > 3)
+                if (input < 1 || input > 3)
                 {
                     Console.WriteLine("rossz input");
                 }
                 else
                 {
-                    if (input == 1) 
+                    if (input == 1)
                     {
-                        CaseManager.
+                        caseManager.ListCase();
                     }
                     else if (input == 2)
                     {
                         Console.Clear();
-                        Console.Write("")
+                        Console.Write("add meg az azonosítót: ");
+                        int azonosito = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("add meg a címet: ");
+                        string cim = Console.ReadLine();
+                        Console.Write("add meg a leírást: ");
+                        string leiras = Console.ReadLine();
+
+                        caseManager.CreateCase(new Case(azonosito, cim, leiras));
+
+                        Console.WriteLine("ugy hozzáadva.\n folytatás [ENTER]");
+                        Console.ReadLine();
+                    }
+                    else if (input == 3)
+                    {
+                        fut = false;
                     }
 
-			}
-            
-		}
+                }
+
+            }
+        }
+        static void szemelyhozzaad()
+        {
+            Console.Clear();
+            Console.WriteLine("Személy hozzaadása ügyhöz\n");
+            Console.Write("ügy azaonsítószáma: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("személy neve: ");
+            string nev = Console.ReadLine();
+            Console.Write("személy kora: ");
+            int eletkor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("személyről megjedzés: ");
+            string megjedzes = Console.ReadLine();
+
+            caseManager.AddPersonToCase(id, new Person(nev, eletkor, megjedzes));
+            Console.WriteLine("\nszemély hozzáadva.\n\nFolyataás [ENTER]");
+        }
         static void Main(string[] args)
         {
-            
-			//Case ugy1 = new Case()
+            Console.Clear();
+
+                //Case ugy1 = new Case()
 
 
 
-			bool program = true;
-            while (program == true)
-            {
-				Console.WriteLine("""
+                bool program = true;
+                while (program == true)
+                {
+                    Console.WriteLine("""
 
                 1. Ügyek kezelése 
                 2. Személyek kezelése 
@@ -62,40 +94,44 @@ namespace Digitalis_Nyomozas
                 5. Elemzés / döntések 
                 6. Kilépés
                 """);
-				int input = Convert.ToInt32(Console.ReadLine());
+                    int input = Convert.ToInt32(Console.ReadLine());
 
-				if (input == 6)
-				{
-                    program = false;
+                    if (input == 6)
+                    {
+                        program = false;
+                    }
+                    else
+                    {
+                        if (input > 6 || input < 1)
+                        {
+                            Console.WriteLine("rossz input!");
+                        }
+                        if (input == 1)
+                        {
+                            ugykezeles();
+                        }
+                        else if (input == 2)
+                        {
+
+
+                        }
+                        else if (input == 3)
+                        {
+
+                        }
+                        else if (input == 4)
+                        {
+
+                        }
+                        else if (input == 5)
+                        {
+
+                        }
+                    }
                 }
-                else
-                {
-                    if(input > 6 || input <1)
-                    {
-                        Console.WriteLine("rossz input!");
-                    }
-                    if (input == 1)
-                    {
-                        ugykezeles();
-                    }
-                    else if(input == 2)
-                    {
 
-                    }else if (input == 3)
-                    {
 
-                    }
-                    else if (input == 4)
-                    {
-
-                    }else if (input == 5)
-                    {
-
-                    }
-                }
-			}
             
-
         }
     }
 }
